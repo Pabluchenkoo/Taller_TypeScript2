6cf64ff964c71180cd8e2f44aebb74be3a939f8a
Pablo <jp.junco@uniandes.edu.co>
2022-10-25 22:12:56 -0500

    first things first

---
+++ Serie.ts
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+export class Serie{
+
+
+    constructor(public id: number, public name: String, public channel: String, public seasons : number,
+                public synopsis: String, public wtw:String, public image : String)
+    {
+        
+    }
+
+
+
+
+
+}
---
+++ scripts/Serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, name, channel, seasons, synopsis, wtw, image) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.synopsis = synopsis;
+        this.wtw = wtw;
+        this.image = image;
+    }
+    return Serie;
+}());
+export { Serie };
